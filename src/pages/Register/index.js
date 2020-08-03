9ac7fd10dbcf1d5cc3701a56a898b8cb67a21ae0
Italo M <italomarcos0010@gmail.com>
2020-08-03 12:50:10 -0300

    inspecionando bug na margem

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -80,6 +80,7 @@ export default function Register() {
                   flexDirection: 'row',
                   width: '100%',
                   height: 80,
+                  justifyContent: 'center',
                   backgroundColor: '#f8f',
                 }}
               >
